b1d093e8500fe0e909853f6c81e5b9b0a3f96c9d
Artiom Gusanu <artyy.gus@gmail.com>
2025-02-06 22:11:26 +0000

    correction error 500

--- db/connection.php
+++ db/connection.php
@@ -1,9 +1,9 @@
 <?php
 // Database connection settings
-$host = 'localhost'; // or your database host
-$dbUsername = 'root'; // your database username
-$dbPassword = 'root123'; // your database password
-$dbName = 'rita'; // your database name
+$host = 'sql200.infinityfree.com'; // or your database host
+$dbUsername = 'if0_38172004'; // your database username
+$dbPassword = 'Siew2vbqSEIUR'; // your database password
+$dbName = 'if0_38172004_rita'; // your database name
 
 // Create connection
 $conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
