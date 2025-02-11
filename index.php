27b3d4cb355463507a9e4f2662e33fd3c21d6a05
Artiom Gusanu <artyy.gus@gmail.com>
2025-02-11 21:17:33 +0000

    css structure (separation by sections)

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ if ($result = mysqli_query($conn, $query)) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rita Cardoso</title>
-    <link rel="stylesheet" href="./styles/home.css">
+    <link rel="stylesheet" href="./styles/import.css">
     <link rel="icon" type="image/png" href="./images/logo/logoRC.png">
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500;700&display=swap"
         rel="stylesheet">
